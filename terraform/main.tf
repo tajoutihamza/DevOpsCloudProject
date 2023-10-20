5ae8689a9e9f34372fe091bba8166d7d694c50a4
Hamza Taoujouti <hamza.taoujouti@sofrecom.com>
2023-10-20 09:14:21 +0100

    create the secondary machines

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,70 +1,102 @@
-# Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
-
-  required_version = ">= 1.1.0"
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
 }
 
-provider "azurerm" {
-  features {}
+resource "azurerm_resource_group" "rg_k8s" {
+  name     = random_pet.rg_name.id
+  location = var.resource_group_location
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "K8SRG"
-  location = "centralus"
+resource "random_pet" "azurerm_vn_name" {
+  prefix = "vnet"
 }
 
-resource "azurerm_virtual_network" "vn" {
-  name                = "k8s-network"
+# Create virtual network
+resource "azurerm_virtual_network" "vn_k8s" {
+  name                = random_pet.azurerm_vn_name.id
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg_k8s.location
+  resource_group_name = azurerm_resource_group.rg_k8s.name
 }
 
-resource "azurerm_subnet" "sn" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vn.name
+resource "random_pet" "sub_name" {
+  prefix = "sub"
+}
+
+# Create subnet
+resource "azurerm_subnet" "sub_k8s" {
+  name                 = random_pet.sub_name.id
+  resource_group_name  = azurerm_resource_group.rg_k8s.name
+  virtual_network_name = azurerm_virtual_network.vn_k8s.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "k8s-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# Create public IPs
+resource "azurerm_public_ip" "second_public_ip" {
+  count               = 2
+  name                = "secondPublicIP${count.index}"
+  location            = azurerm_resource_group.rg_k8s.location
+  resource_group_name = azurerm_resource_group.rg_k8s.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "nic_k8s" {
+  count               = 2
+  name                = "secondNIC${count.index}"
+  location            = azurerm_resource_group.rg_k8s.location
+  resource_group_name = azurerm_resource_group.rg_k8s.name
 
   ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.sn.id
+    name                          = "IpConfiguration"
+    subnet_id                     = azurerm_subnet.sub_k8s.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.second_public_ip[count.index].id
   }
 }
 
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                            = "master-machine"
-  resource_group_name             = azurerm_resource_group.rg.name
-  location                        = azurerm_resource_group.rg.location
-  size                            = "Standard_B2s"
-  admin_username                  = "adminuser"
-  admin_password                  = "Azurek8s+2023"
-  disable_password_authentication = "false"
-  network_interface_ids = [
-    azurerm_network_interface.nic.id,
-  ]
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+resource "azurerm_network_interface_security_group_association" "second_nic_sga" {
+  count                     = 2
+  network_interface_id      = azurerm_network_interface.nic_k8s[count.index].id
+  network_security_group_id = azurerm_network_security_group.master_nsg.id
+}
+
+resource "random_pet" "azurerm_linux_vm_name" {
+  prefix = "vm"
+}
+
+resource "azurerm_linux_virtual_machine" "vm_k8s" {
+  count                 = 2
+  name                  = "${random_pet.azurerm_linux_vm_name.id}${count.index}"
+  location              = azurerm_resource_group.rg_k8s.location
+  resource_group_name   = azurerm_resource_group.rg_k8s.name
+  network_interface_ids = [azurerm_network_interface.nic_k8s[count.index].id]
+  size                  = "Standard_B2s"
+  # delete_data_disks_on_termination = true
+  # delete_os_disk_on_termination = true
 
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-focal"
     sku       = "20_04-lts"
     version   = "latest"
+  }
+  # admin_ssh_key {
+  #   username   = var.username
+  #   public_key = jsondecode(azapi_resource_action.ssh_public_key_gen.output).public_key
+  # }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "secondosdisks${count.index}"
+  }
+
+  computer_name  = "secondk8s${count.index}"
+  admin_username = var.username
+  admin_password                  = var.password
+  disable_password_authentication = "false"
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.master_account.primary_blob_endpoint
   }
 }
