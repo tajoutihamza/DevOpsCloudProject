ca11e16a4968cba92325d28c1892560527a6ef8e
Hamza Taoujouti <hamza.taoujouti@sofrecom.com>
2023-12-04 16:21:12 +0100

    add dynamic local storage provisioner and kubernetes dashbord, its service account, roles and secret to access it,

---
+++ scripts/KubeDashStorageSetup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
+git clone https://github.com/rancher/local-path-provisioner.git
+cd local-path-provisioner
+helm install local-path-storage --create-namespace --namespace local-path-storage ./deploy/chart/local-path-provisioner/
