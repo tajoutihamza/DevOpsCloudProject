27e4cb907c4a7472e41e104776ec6e52e3b581cd
Hamza Taoujouti <hamza.taoujouti@sofrecom.com>
2023-12-04 16:22:24 +0100

    add private ips into outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -19,3 +19,10 @@ output "azurerm_linux_vm_ip_address" {
 output "public_ip_address" {
   value = azurerm_linux_virtual_machine.master_vm.public_ip_address
 }
+output "master_private_ip_address" {
+  value =  azurerm_linux_virtual_machine.master_vm.private_ip_address
+}
+
+output "private_ip_address" {
+  value =  [for s in azurerm_linux_virtual_machine.vm_k8s : s.private_ip_address]
+}
