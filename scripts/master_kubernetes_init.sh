1e6fad3d6c11a5d7044dba06ceb2c7cc10d32765
Hamza Taoujouti <hamza.taoujouti@sofrecom.com>
2023-12-04 16:16:21 +0100

    add ip to publish control plane endpoint in case we need a multi-master k8s cluster

--- scripts/master_kubernetes_init.sh
+++ scripts/master_kubernetes_init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo kubeadm config images pull
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16
-mkdir -p $HOME/.kube
+PRIVATEIP=$(hostname -I | awk '{print $1}')
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16  --upload-certs --control-plane-endpoint "${PRIVATEIP}:6443"
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
@@ -9,3 +9,4 @@ kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/
 # sudo kubeadm join &lt;MASTER_NODE_IP>:&lt;API_SERVER_PORT> --token &lt;TOKEN> --discovery-token-ca-cert-hash &lt;CERTIFICATE_HASH>
 # kubectl get no 
 #add workers to /etc/hosts
+#scp ~/.kube/config secondk8s1:~/.kube/config
